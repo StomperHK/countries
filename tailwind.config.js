91625192791e5cd27edd3f89a5639ecb9c693096
Rafael Ribeiro <ra20fael04@gmail.com>
2024-11-13 19:00:54 -0300

    feat: create search bar element and style.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,9 +4,12 @@ module.exports = {
   darkMode: "selector",
   theme: {
     extend: {
+      size: {
+        "1.5": "0.375rem",
+      },
       boxShadow: {
         "card-hover": "0px 0px 0px 11px var(--shadow-color)",
-        "focus-visible": "0 0 0 6px var(--shadow-color)",
+        "focus-visible": "0 0 0 4px var(--shadow-color)",
       },  
       gridTemplateColumns: {
         "countries": "repeat(auto-fill, minmax(220px, 1fr))",
