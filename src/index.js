2851fb6f735091875bf23478dcec963be53abdc0
Rafael Ribeiro <ra20fael04@gmail.com>
2024-11-13 19:01:30 -0300

    feat: implement search bar.

--- src/index.js
+++ src/index.js
@@ -2,20 +2,55 @@ import "./themeSwitcher.js";
 import {setCountryModalState, populateAndShowCountryDialog} from "./countryModal.js";
 import "/index.css";
 
+const searchBar = document.querySelector('[data-js="search-bar"]')
+const searchBarClearButton = document.querySelector('[data-js="search-bar-clear"]')
+
 const loadingObserverEL = document.querySelector('[data-js="loading-observer"]')
 let loadingObserver = null
 
-const apiBaseURL = "https://restfulcountries.com/api/v1"
+const apiBaseURL = "https://restfulcountries.com/api/v1/countries"
 const apiToken = import.meta.env.VITE_API_TOKEN
 let currentPage = 0;
 let reachedEnd = false;
 
 
-async function requestCountries() {  
+function setCountryListState(state) {
+  hideAllListContainers()
+
+  switch (state) {
+    case "success":
+      document.querySelector('[data-js="countries-list-content"]').classList.remove('opacity-0')
+      break;
+      
+    case "loading":
+      document.querySelector('[data-js="main-spinner"]').classList.remove('opacity-0');
+      break;
+    case "error":
+      document.querySelector('[data-js="countries-list-error"]').classList.remove('hidden')
+      break;
+    case "empty":
+      document.querySelector('[data-js="countries-list-empty"]').classList.remove('hidden')
+      break;
+    default:
+      throw new Error(`Unknown state: ${state}`)
+  }
+}
+
+function hideAllListContainers() {
+  [...document.querySelectorAll('[data-js="countries-list-wrapper"] > div')].slice(1).forEach(container => container.classList.add("hidden"))
+
+  document.querySelector('[data-js="countries-list-content"]').classList.add('opacity-0')
+  document.querySelector('[data-js="main-spinner"]').classList.add("opacity-0")
+}
+
+async function requestCountries(isInfiniteScrollFetching = false) {
+  setCountryListState("loading")
+  if (isInfiniteScrollFetching) document.querySelector('[data-js="countries-list-content"]').classList.remove('opacity-0')
+
   try {
     if (reachedEnd) return
 
-    const response = await fetch(`${apiBaseURL}/countries?per_page=10&page=${currentPage}`, {
+    const response = await fetch(`${apiBaseURL}?per_page=10&page=${currentPage}`, {
       headers: {
         Authorization: `Bearer ${apiToken}`
       }
@@ -29,7 +64,7 @@ async function requestCountries() {
 
     if (!data) {
       reachedEnd = true
-      deleteLoadingObserer()
+      deleteLoadingObserver()
 
       return
     }
@@ -47,11 +82,19 @@ async function requestCountries() {
 
 requestCountries()
 
-function tokenizeAndAppendData(data) {
-  const countries = data
-  const countriesList = document.querySelector('[data-js="countries-list"]')
+function tokenizeAndAppendData(data, clearData = false) {
+  let countries = data
+  const countriesList = document.querySelector('[data-js="countries-list-content"]')
   const fragment = document.createDocumentFragment()
   const cardTemplate = document.querySelector('[data-js="card-template"]')
+
+  if (clearData) {
+    countriesList.innerHTML === ""
+  }
+
+  setCountryListState("success")
+
+  if (!Array.isArray(countries)) countries = [countries]
 
   countries.forEach(country => {
     const { name, size, continent, population, href: { self, flag } } = country
@@ -76,7 +119,44 @@ function tokenizeAndAppendData(data) {
   countriesList.appendChild(fragment)
 }
 
-tokenizeAndAppendData([{
+function debounceRequestCountry() {
+  let timer
+
+  return function () {
+    const searchBarValue = searchBar.value
+    
+    checkSearchBarValue(searchBarValue)
+    clearTimeout(timer)
+    setCountryListState("loading")
+
+    timer = setTimeout(() => {
+      const countriesList = document.querySelector('[data-js="countries-list-content"]')
+      countriesList.innerHTML = ""
+
+      requestCountry(apiBaseURL + "/" + searchBarValue.trim(), "search-bar").then(data => data !== 404 ? tokenizeAndAppendData(data, true) : setCountryListState("empty"))
+      deleteLoadingObserver()
+    }, 1000)
+  }
+}
+
+function checkSearchBarValue(searchBarValue) {
+  if (!searchBarValue) searchBarClearButton.classList.add("invisible")
+  else searchBarClearButton.classList.remove("invisible")
+}
+
+function clearSeachBarAndRequestCountries() {
+  const countriesList = document.querySelector('[data-js="countries-list-content"]')
+  countriesList.innerHTML = ""
+  searchBar.value = ""
+  currentPage = 0
+  reachedEnd = false
+
+  this.classList.add("invisible")
+
+  requestCountries()
+}
+
+/*tokenizeAndAppendData([{
   "name": "Switzerland",
   "full_name": "Swiss Confederation",
   "capital": "Berne",
@@ -126,9 +206,12 @@ tokenizeAndAppendData([{
     "presidents": "https://restfulcountries.com/api/v1/countries/Switzerland/presidents",
     "flag": "https://restfulcountries.com/assets/images/flags/Switzerland.png"
   }
-}])
+}])*/
 
-async function requestCountry(countrySelfUrl) {
+async function requestCountry(countrySelfUrl, componentThatMadeTheRequest) {
+  if (componentThatMadeTheRequest === "search-bar") setCountryListState("loading")
+  else setCountryModalState("loading")
+
   try {
     const response = await fetch(countrySelfUrl, {
       headers: {
@@ -137,15 +220,20 @@ async function requestCountry(countrySelfUrl) {
     })
 
     if (!response.ok) {
-      throw new Error('Failed to fetch data')
+      throw new Error(response.status)
     }
+
+    setCountryListState("success")
 
     const data = (await response.json()).data
     
     return data
 
-  } catch (error) {
-    console.log(error);
+  } catch (errorCode) {
+    if (errorCode.message == 404) return 404
+
+    if (componentThatMadeTheRequest === "search-bar") setCountryListState("error")
+    else setCountryModalState("error")
   }
 }
 
@@ -154,15 +242,21 @@ function createLoadingObserver() {
 
   loadingObserver = new IntersectionObserver(([entry]) => {
     if (entry.isIntersecting) {
-      requestCountries()
+      requestCountries(true)
     }
   })
 
   loadingObserver.observe(loadingObserverEL)
 }
 
-function deleteLoadingObserer() {
-  loadingObserverEL.textContent = "you reached the end :O"
-
+function deleteLoadingObserver() {
   loadingObserver.unobserve(loadingObserverEL)
+  loadingObserver = null
+
+  loadingObserverEL.textContent = "you reached the end :O"
 }
+
+
+searchBar.addEventListener('input', debounceRequestCountry())
+
+searchBarClearButton.addEventListener('click', clearSeachBarAndRequestCountries)
